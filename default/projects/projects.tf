cf4a1fe0518125618b2e165c278fd6e409184c16
Diego OJ <diego.ojeda@binbash.com.ar>
2022-01-06 10:47:46 -0300

    Reconcile renamed Ref Arch repo and add a new sample repo

--- default/projects/projects.tf
+++ default/projects/projects.tf
@@ -7,3 +7,24 @@ resource "gitlab_project" "my_super_test_project" {
 
   remove_source_branch_after_merge = true
 }
+
+resource "gitlab_project" "le_tf_sandbox" {
+  name             = "Leverage Terraform Ref Arch Sandbox"
+  path             = "le-tf-infra-aws-sandbox"
+  description      = "Just for testing Terraform + GitLab"
+  namespace_id     = gitlab_group.binbash_leverage.id
+  visibility_level = "private"
+
+  only_allow_merge_if_pipeline_succeeds = true
+  remove_source_branch_after_merge      = true
+}
+
+resource "gitlab_project" "le_sample_backend_app" {
+  name             = "Leverage Sample Backend App"
+  path             = "le-sample-backend-app"
+  description      = "A sample backend application"
+  namespace_id     = gitlab_group.binbash_leverage.id
+  visibility_level = "private"
+
+  remove_source_branch_after_merge = true
+}
